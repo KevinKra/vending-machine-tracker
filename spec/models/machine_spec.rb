8f86f1ebfd57510f6b2ad1aeb1123a2446ea9f93
Kevin Krato <kratok08@gmail.com>
2019-12-19 10:04:25 -0700

    Test and define machine average_snack_cost method

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -10,4 +10,27 @@ RSpec.describe Machine, type: :model do
     it { should have_many(:machine_snacks) }
     it { should have_many(:snacks).through(:machine_snacks) }
   end
+
+  # instance methods
+  describe 'methods' do
+    it "can calculate the average cost of its snacks" do
+      @owner_1 = Owner.create(
+        name: "Bob"
+      )
+      @machine_1 = Machine.create(
+        location: "Iowa",
+        owner_id: @owner_1.id
+      )
+      @snack_1 = Snack.create(
+        name: "M&Ms",
+        cost: 200
+      )
+      @snack_2 = Snack.create(
+        name: "Skittles",
+        cost: 100
+      )
+      @machine_1.snacks << [@snack_1, @snack_2]
+      expect(@machine_1.average_snack_cost).to eq(150)
+    end
+  end
 end
