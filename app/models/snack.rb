f5c66b1252b3f1e3ba74e692878b9a31227fa3c5
Kevin Krato <kratok08@gmail.com>
2019-12-19 10:37:27 -0700

    Turn in for PR

--- app/models/snack.rb
+++ app/models/snack.rb
@@ -3,4 +3,8 @@ class Snack < ApplicationRecord
 
   has_many :machine_snacks
   has_many :machines, through: :machine_snacks
+
+  def find_all_locations
+    @locations = machines.pluck(:location)
+  end
 end
--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -10,4 +10,30 @@ RSpec.describe Snack, type: :model do
     it { should have_many(:machine_snacks) }
     it { should have_many(:machines).through(:machine_snacks) }
   end
+
+  # instance methods
+  describe 'instance methods' do
+    before {
+      @owner_1 = Owner.create(
+        name: "Bob"
+      )
+      @machine_1 = Machine.create(
+        location: "Iowa",
+        owner_id: @owner_1.id
+      )
+      @snack_1 = Snack.create(
+        name: "M&Ms",
+        cost: 200
+      )
+      @snack_2 = Snack.create(
+        name: "Skittles",
+        cost: 100
+      )
+      @machine_1.snacks << [@snack_1, @snack_2]
+    }
+    
+    it "can report the location" do
+      expect(@snack_1.find_all_locations).to eq(["Iowa"])
+    end
+  end
 end
